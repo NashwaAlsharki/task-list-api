3e52680ce16695567d6c71f2adf5e41f1761f5d1
Steffanie Lopez <lopezsteffanie8@gmail.com>
2022-11-03 11:14:34 -0700

    began working on wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,40 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response
+
+task_bp = Blueprint('task_bp', __name__, url_prefix='/task')
+
+@task_bp.route("", methods=["GET"])
+def all_tasks():
+
+
+@task_bp.route("/<task_id>", methods=["GET"])
+
+
+@task_bp.route("/<title>", methods=["GET"])
+
+
+@task_bp.route("/<completed_at>", methods=["GET"])
+
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"]
+    )
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    # Successful response
+    return make_response(f"Task {new_task.title} has been successfully created!", 201)
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -60,13 +60,12 @@ def test_get_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
+    assert response_body == {"message": "Task not found"}, 404
 
-
-@pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task(client):
     # Act
     response = client.post("/tasks", json={
@@ -131,11 +130,11 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert response_body == {"message": "Task not found"}, 404
 
 @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task(client, one_task):
@@ -161,11 +160,11 @@ def test_delete_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert response_body == {"message": "Task not found"}, 404
     assert Task.query.all() == []
 
 
