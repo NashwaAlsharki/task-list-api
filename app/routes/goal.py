91af0be1d3123d31931d46f62deacdbdf62b91ea
Steffanie Lopez <lopezsteffanie8@gmail.com>
2022-11-09 06:36:28 -0800

    passed wave 5 tests

--- app/routes/goal.py
+++ app/routes/goal.py
@@ -1,29 +1,30 @@
 from flask import Blueprint, abort, jsonify, make_response, request
 from app.models.goal_model import Goal
-from .helper import get_by_id
+from .helper import get_goal_by_id
 from app import db
 
 goal_bp = Blueprint('goal_bp', __name__, url_prefix='/goals')
 
 # take a goal and return it as a dictionary
 def json_details(goal):
-    return {
-            "id": goal.goal_id,
-            "title": goal.title,
+    return {"goal": {
+                "id": goal.goal_id,
+                "title": goal.title,
             }
+        }
 
 # return all goals as json
 @goal_bp.route("", methods=["GET"])
 def get_goal():
     goals = Goal.query.all()
-    response = [json_details(goal) for goal in goals]
+    response = [{"id": goal.goal_id, "title": goal.title} for goal in goals]
     
     return jsonify(response), 200
     
 # return one goal by id
 @goal_bp.route("/<goal_id>", methods=["GET"])
 def get_one_goal(goal_id):
-    goal = get_by_id(Goal, goal_id)
+    goal = get_goal_by_id(Goal, goal_id)
     return json_details(goal), 200
 
 # create a new goal
@@ -43,17 +44,17 @@ def create_goal():
 # delete a goal by id
 @goal_bp.route("/<goal_id>", methods=["DELETE"])
 def delete_goal(goal_id):
-    goal = get_by_id(Goal, goal_id)
+    goal = get_goal_by_id(Goal, goal_id)
 
     db.session.delete(goal)
     db.session.commit()
 
-    return {"message": f"goal {goal.goal_id} successfully deleted"}, 200
+    return {"details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}, 200
 
 # update a goal by id
 @goal_bp.route("/<goal_id>", methods=["PUT"])
 def update_task(goal_id):
-    goal = get_by_id(Goal, goal_id)
+    goal = get_goal_by_id(Goal, goal_id)
     request_body = request.get_json()
 
     goal.title = request_body["title"]
